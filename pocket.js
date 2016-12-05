0bcb557c3eec982091e317f285452648d563ceb6
Supratim Chowdhury <supratims@gmail.com>
2016-12-05 14:29:27 +0000

    pocket auth and post code

--- pocket.js
+++ pocket.js
@@ -5,12 +5,40 @@
 var request = require('request');
 var properties = require('properties');
 
-function post(data, success_callback, error_callback){
-	
+// Auth using pocket api and return a access_token
+// https://getpocket.com/developer/docs/authentication
+function auth(consumer_key){
+	if (!consumer_key) {
+		consumer_key = properties.get('consumer_key');
+	}
+	request.post(
+		'https://getpocket.com/v3/oauth/request',
+		{
+			consumer_key: consumer_key, 
+			redirect_uri: 'pocketapp_bot:authorizationFinished'
+		},
+		function(error, response, body){
+			if (error){
+				throw error;
+			}			
+			return response.code;	
+		}
+	);
+}
 
+// https://getpocket.com/developer/docs/v3/add
+function post(access_token, url, tweet_id, success_callback, error_callback){
+	if (!access_token){
+		throw new Error('No access token found. Call auth() first.');
+	}
 	request.post(
 	    'https://getpocket.com/v3/add',
-	    data,
+	    {
+		url: url,
+		tweet_id: tweet_id,
+		consumer_key: properties.get('consumer_key'),
+		access_token: access_token	
+	    },
 	    function (error, response, body) {
 		if (error){
 			error_callback && error_callbacr(error);
