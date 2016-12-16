60371e6e5a62952aa074fb6f930595614741172f
Supratim Chowdhury <supratims@gmail.com>
2016-12-16 15:21:09 +0000

    use PORT env

--- server.js
+++ server.js
@@ -12,9 +12,12 @@ if ('development' == env) {
 app.get('/', function(req, res){
 	res.send(fs.readFileSync('./retweets.json'));
 });
+app.get('/last', function(req, res){
+        res.send(fs.readFileSync('./last_retweet'));
+});
 
-app.listen(process.env.NODE_PORT || 3000);
-console.log('Listening on port ' + (process.env.NODE_PORT || 3000));
+app.listen(process.env.PORT || 3000);
+console.log('Listening on port ' + (process.env.PORT || 3000));
 
 var poll_user = require('./poll_user');
 poll_user.run();
