412c8c99a5daa9ac828e648e2fc569aa44a4e79c
Supratim Chowdhury <supratims@gmail.com>
2016-12-20 14:30:37 +0000

    protocol change and last tweet id update

--- ping_app.js
+++ ping_app.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 function run(){
 	setInterval(function() {
-	    http.get("https://ancient-harbor-69037.herokuapp.com/");
+	    http.get("http://ancient-harbor-69037.herokuapp.com/");
 	}, 300000); // every 15 minutes (300000)
 }
 
